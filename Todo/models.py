d251db54b18a2827daf17c25f62b61963113b6af
Tanvir Kabir <tanvirkabir47@gmail.com>
2024-04-12 10:42:16 +0600

    update

--- Todo/models.py
+++ Todo/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Todo(models.Model):
     title = models.CharField(max_length=150)
     message = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created_at',]
     
     
     def __str__(self):
